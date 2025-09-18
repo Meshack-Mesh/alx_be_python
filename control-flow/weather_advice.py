68419e559262a90745a61c68beb6f5c77fc55412
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-09-18 16:45:14 +0300

    Control Flow Tasks

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
-weather = input("What's the weathe like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 if weather == "sunny":
-   print("wear a t-shirt and sunglasses.")
+   print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
