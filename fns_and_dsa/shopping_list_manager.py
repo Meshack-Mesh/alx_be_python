7ca2a8fd55f565000a60b88af07007e96f90577e
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-09-25 18:31:46 +0300

    Functions

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,8 +13,8 @@ def display_menu():
             choice = input("Enter your choice")
 
             if choice == '1':
-                item = input ("Enter the item to add: ").strip()
-                if item: #mkae sure the item is not empty
+                item = input ("Enter the item to add: ")
+                if item: #make sure the item is not empty
                     shopping_list.append(item)
                     print(f"'{item}' has been added to the list.")
                 else:
