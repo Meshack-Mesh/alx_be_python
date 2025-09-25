239a8ddbc76a2124042070bf237c8e4c41833222
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-09-25 18:20:15 +0300

    Functions

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 def display_current_datetime():
-    print("Current date and time:", current_date.strftime("%Y-5m-%d %H:%M:%S"))
+    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
     return display_current_datetime
 def calculate_future_date(days_to_add: int):
 
